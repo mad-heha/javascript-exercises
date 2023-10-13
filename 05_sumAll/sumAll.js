7693b182ee27aea52a305498850f9c4c28dffd09
Mad_Heha <38478352+mad-heha@users.noreply.github.com>
2023-10-13 11:16:58 -0400

    Finished 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //checks if a or b are integers
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    //checks if a or b or not negative numbers
+    if(a < 0 || b < 0) return "ERROR";
+    //switches if a is bigger than b
+    if(a > b){
+        const temp = a;
+        a = b;
+        b = temp;
+    }
 
+    //adds digits between (and including) a and b
+    let sum = 0;
+    for (let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
